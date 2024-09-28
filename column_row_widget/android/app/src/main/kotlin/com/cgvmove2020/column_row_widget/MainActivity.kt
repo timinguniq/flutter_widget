1a20b02812a2cee67af3b96a1a614f3256f935fe
timing.uniq <timing.uniq@gmail.com>
2024-09-28 21:22:51 +0900

    feat: init

--- column_row_widget/android/app/src/main/kotlin/com/cgvmove2020/column_row_widget/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.cgvmove2020.column_row_widget
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ column_widget/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text('Hello, World!'),
+              Text('Hello, World!'),
+              Text('Hello, World!'),
+              Text('Hello, World!'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
