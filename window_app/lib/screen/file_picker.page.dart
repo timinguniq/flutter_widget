24674a546862fec34dd0b5e8b4fcdbf5e2686c16
leejh <timing.uniq@gmail.com>
2024-11-17 18:10:46 +0900

    feat: windows app

---
+++ window_app/lib/screen/file_picker.page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FilePickerPage extends StatelessWidget {
+  const FilePickerPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('테스트')
+    );
+  }
+}
